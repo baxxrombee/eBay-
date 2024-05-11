c64230dcfa54b28dd377273ca2e2c95017fbcecd
baxxrombee <sayfullayevbaxxrombee@gmail.com>
2024-05-11 18:18:30 +0500

    first commit

---
+++ src/redux/Store.js
@@ -0,0 +1,11 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import likeReducer from './SliceReducer';
+import cartReducer from './cartSlice';
+
+export const store = configureStore({
+  reducer: {
+    like: likeReducer,
+    cart: cartReducer 
+  }
+});
